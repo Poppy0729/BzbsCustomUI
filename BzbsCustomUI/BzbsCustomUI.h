58f7e5a22746c40048959bc4da52879f0da67bb1
Saowalak Rungrat <saowalak.r@buzzebees.com>
2022-10-10 15:53:42 +0700

    Prepare framework

---
+++ BzbsCustomUI/BzbsCustomUI.h
@@ -0,0 +1,18 @@
+//
+//  BzbsCustomUI.h
+//  BzbsCustomUI
+//
+//  Created by Saowalak Rungrat on 10/10/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BzbsCustomUI.
+FOUNDATION_EXPORT double BzbsCustomUIVersionNumber;
+
+//! Project version string for BzbsCustomUI.
+FOUNDATION_EXPORT const unsigned char BzbsCustomUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BzbsCustomUI/PublicHeader.h>
+
+
